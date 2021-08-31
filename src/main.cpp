e3764847425860d2213c605c7fbbffd4e5c7017c
nexovec <hraccesky@gmail.com>
2021-08-31 23:58:16 +0200

    rasterized triangle with color gradient using barycentric cordinates

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ struct Triangle_2D
     Vec_2f v2;
     Vec_2f v3;
 };
-// ? TODO: make vertex struct
 template <typename T>
 T Sgn(T, T = 0);
 template <typename T>
@@ -39,43 +38,44 @@ void clear_screen(Back_buffer back_buffer)
         }
     }
 }
-unsigned int pixel_program(Triangle_2D triangle, int x, int y, unsigned int color1, unsigned int color2, unsigned int color3)
+unsigned int pixel_program(Triangle_2D triangle, float x, float y, unsigned int color1, unsigned int color2, unsigned int color3)
 {
     // use solid color
     // return 0xff00ff00;
+    Vec_2f v1 = triangle.v1;
+    Vec_2f v2 = triangle.v2;
+    Vec_2f v3 = triangle.v3;
+    // get barycentric coordinates
+    float det_t = (v2.y - v3.y) * (v1.x - v3.x) + (v3.x - v2.x) * (v1.y - v3.y);
+    float lam_1 = ((v2.y - v3.y) * (x - v3.x) + (v3.x - v2.x) * (y - v3.y)) / det_t;
+    float lam_2 = ((v3.y - v1.y) * (x - v3.x) + (v1.x - v3.x) * (y - v3.y)) / det_t;
+    float lam_3 = 1 - lam_2 - lam_1;
 
-    // TODO: use barycentric
+    unsigned char a1 = (color1 >> 24);
+    unsigned char r1 = (color1 >> 16);
+    unsigned char g1 = (color1 >> 8);
+    unsigned char b1 = (color1 >> 0);
 
-    // lerp color along y-axis only:
-    unsigned int a1 = (color1 >> 24);
-    unsigned int r1 = (color1 >> 16) & 0xff;
-    unsigned int g1 = (color1 >> 8) & 0xff;
-    unsigned int b1 = (color1 >> 0) & 0xff;
+    unsigned char a2 = (color2 >> 24);
+    unsigned char r2 = (color2 >> 16);
+    unsigned char g2 = (color2 >> 8);
+    unsigned char b2 = (color2 >> 0);
 
-    unsigned int a2 = (color2 >> 24);
-    unsigned int r2 = (color2 >> 16) & 0xff;
-    unsigned int g2 = (color2 >> 8) & 0xff;
-    unsigned int b2 = (color2 >> 0) & 0xff;
+    unsigned char a3 = (color3 >> 24);
+    unsigned char r3 = (color3 >> 16);
+    unsigned char g3 = (color3 >> 8);
+    unsigned char b3 = (color3 >> 0);
 
-    unsigned int a3 = (color3 >> 24);
-    unsigned int r3 = (color3 >> 16) & 0xff;
-    unsigned int g3 = (color3 >> 8) & 0xff;
-    unsigned int b3 = (color3 >> 0) & 0xff;
-
-    unsigned int final_a = (unsigned int)Lerp(a1, a2, InvLerp(triangle.v1.y, triangle.v2.y, y));
-    unsigned int final_r = (unsigned int)Lerp(r1, r2, InvLerp(triangle.v1.y, triangle.v2.y, y));
-    unsigned int final_g = (unsigned int)Lerp(g1, g2, InvLerp(triangle.v1.y, triangle.v2.y, y));
-    unsigned int final_b = (unsigned int)Lerp(b1, b2, InvLerp(triangle.v1.y, triangle.v2.y, y));
+    unsigned char final_a = a1 * lam_1 + a2 * lam_2 + a3 * lam_3;
+    unsigned char final_r = r1 * lam_1 + r2 * lam_2 + r3 * lam_3;
+    unsigned char final_g = g1 * lam_1 + g2 * lam_2 + g3 * lam_3;
+    unsigned char final_b = b1 * lam_1 + b2 * lam_2 + b3 * lam_3;
 
     // return ((final_a&0xff) << 24) + ((final_r&0xff) << 16) + ((final_g&0xff) << 8) + final_b&0xff; // <- DEBUG
     return (final_a << 24) + (final_r << 16) + (final_g << 8) + final_b;
 }
-void GameUpdateAndRender(Back_buffer back_buffer)
+void RasterizeTriangle(Back_buffer back_buffer)
 {
-    // NOTE: backbuffer format is ARGB
-    // NOTE: backbuffer.width is scene_width; backbuffer_height is scene_height
-    clear_screen(back_buffer);
-
     // SECTION: generate sample triangle
     Vec_2f x = {200.0f, 400.0f};
     Vec_2f y = {500.0f, 200.0f};
@@ -83,68 +83,66 @@ void GameUpdateAndRender(Back_buffer back_buffer)
     Triangle_2D triangle = {x, y, z};
     // Triangle_2D<float> triangle = {{200.0f, 200.0f}, {500.0f, 200.0f}, {200.0f, 500.0f}};
 
+    // TODO: render wireframe
     // SECTION: rasterize triangle
-    // TODO: rasterize one triangle
+    int scanline_x_start[scene_width] = {}; // should probably be common for all triangles, should use unsigned short[]
+    Vec_2f *vertices = (Vec_2f *)&triangle;
+    for (int i1 = 0; i1 < 3; i1++)
     {
-        // TODO: render wireframe
-        int scanline_x_start[scene_width] = {}; // should probably be common for all triangles, should use unsigned short[]
-        Vec_2f *vertices = (Vec_2f *)&triangle;
-        for (int i1 = 0; i1 < 3; i1++)
+        int i2 = (i1 + 1) % 3;
+        Vec_2f v1 = vertices[i1];
+        Vec_2f v2 = vertices[i2];
+        Vec_2f lower_vertex = v1;
+        Vec_2f higher_vertex = v2;
+        if (v1.y > v2.y)
         {
-            int i2 = (i1 + 1) % 3;
-            Vec_2f v1 = vertices[i1];
-            Vec_2f v2 = vertices[i2];
-            Vec_2f lower_vertex = v1;
-            Vec_2f higher_vertex = v2;
-            if (v1.y > v2.y)
+            lower_vertex = v2;
+            higher_vertex = v1;
+        }
+        // ? TODO: benchmark copying of data vs. pointers vs. vertex layouts etc.
+        // TODO: optimize by better type choices
+        for (int y = (int)lower_vertex.y; y < (int)higher_vertex.y; y++)
+        {
+            int relative_y_diff = y - (int)lower_vertex.y;
+            float lerp_unit = (float)1 / (higher_vertex.y - lower_vertex.y);
+            int x_bound = (int)Lerp(lower_vertex.x, higher_vertex.x, lerp_unit * (float)relative_y_diff);
+            // ! TODO: ignore scanlines outside of screenspace
+            if (scanline_x_start[y])
             {
-                lower_vertex = v2;
-                higher_vertex = v1;
-            }
-            Vec_2f left_vertex = v1;
-            Vec_2f right_vertex = v2;
-            if (v1.x > right_vertex.x)
-            {
-                right_vertex = v2;
-                left_vertex = v1;
-            }
-            // ? TODO: benchmark copying of data vs. pointers vs. vertex layouts etc.
-            // TODO: optimize by better type choices
-            for (int y = (int)lower_vertex.y; y < (int)higher_vertex.y; y++)
-            {
-                int relative_y_diff = y - (int)lower_vertex.y;
-                float lerp_unit = (float)1 / (higher_vertex.y - lower_vertex.y);
-                int x = (int)Lerp(lower_vertex.x, higher_vertex.x, lerp_unit * (float)relative_y_diff);
-                // ! TODO: ignore scanlines outside of screenspace
-                if (scanline_x_start[y])
+                // this row has scanline boundary cached for this triangle
+                int lower_x_bound;
+                int higher_x_bound;
+                if (scanline_x_start[y] > x_bound)
                 {
-                    // this row has scanline boundary cached for this triangle
-                    int lower_x_bound;
-                    int higher_x_bound;
-                    if (scanline_x_start[y] > x)
-                    {
-                        lower_x_bound = x;
-                        higher_x_bound = scanline_x_start[y];
-                    }
-                    else
-                    {
-                        higher_x_bound = x;
-                        lower_x_bound = scanline_x_start[y];
-                    }
-                    for (int x = lower_x_bound; x < higher_x_bound; x++)
-                    {
-                        back_buffer(x, y) = pixel_program(triangle, x, y, 0xffff00ff, 0xffffff00, 0xffffffff);
-                    }
-                    scanline_x_start[y] = 0;
+                    lower_x_bound = x_bound;
+                    higher_x_bound = scanline_x_start[y];
                 }
                 else
                 {
-                    // this row doesn't have scanline boundary cached for this triangle
-                    scanline_x_start[y] = x;
+                    higher_x_bound = x_bound;
+                    lower_x_bound = scanline_x_start[y];
                 }
+                for (int x = lower_x_bound; x < higher_x_bound; x++)
+                {
+                    back_buffer(x, y) = pixel_program(triangle, x, y, 0xffff00ff, 0xffffff00, 0xffffffff);
+                }
+                scanline_x_start[y] = 0;
+            }
+            else
+            {
+                // this row doesn't have scanline boundary cached for this triangle
+                scanline_x_start[y] = x_bound;
             }
         }
     }
+}
+void GameUpdateAndRender(Back_buffer back_buffer)
+{
+    // NOTE: backbuffer format is ARGB
+    // NOTE: backbuffer.width is scene_width; backbuffer_height is scene_height
+    clear_screen(back_buffer);
+    RasterizeTriangle(back_buffer);
+
     // TODO: generate triangles that fill screen
     // TODO: rasterize multiple triangles
     // TODO: make a vertex buffer
