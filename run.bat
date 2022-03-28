228b2a876ca0b19a27d456f6a1149c582b7e9484
nexovec <hraccesky@gmail.com>
2022-03-28 15:29:11 +0200

    added run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+pushd %~dp0
+echo Y | rmdir .\bin /s /q
+call .\premake5.exe vs2022
+@REM call MSBuild.exe GLBox.sln -m -t:Clean
+call MSBuild.exe Cpp-software-rasterizer.sln -m
+call .\bin\Debug\Sandbox.exe
+popd
